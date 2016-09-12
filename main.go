c5431a3752a5f2f137c1d3da6e208efa5b9e54dc
Timothy Dang <timothy.dang@gmail.com>
2016-09-12 16:51:22 -0700

    Handled input from url string

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+// Page of the web site
+type Page struct {
+	Name string
+}
+
 func main() {
-	fmt.Println("Hello World")
+	templates := template.Must(template.ParseFiles("templates/index.html"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Go Web")
+		p := Page{Name: "Gopher"}
+
+		// Assigns name of the p object from the url
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		fmt.Fprintf(w, "Hello from func main")
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
