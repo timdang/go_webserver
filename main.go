a264fede0b3157921dba3ca081aeed1d4401a4df
Timothy Dang <timothy.dang@gmail.com>
2016-09-14 15:59:34 -0700

    Adding basic db.

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // Page of the web site
 type Page struct {
-	Name string
+	Name     string
+	DBStatus bool
 }
 
 func main() {
 	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	db, _ := sql.Open("sqlite3", "dev.db")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := Page{Name: "Gopher"}
@@ -21,10 +27,12 @@ func main() {
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
+		p.DBStatus = db.Ping() == nil
+
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		fmt.Fprintf(w, "Hello from func main")
+		// fmt.Fprintf(w, "Hello from func main")
 	})
 	fmt.Println("Serving from port 8080")
 	fmt.Println(http.ListenAndServe(":8080", nil))
